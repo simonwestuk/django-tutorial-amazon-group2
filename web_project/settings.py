29aa9812a8e7b2bcb23f2fc4cc4c243caed0b123
Simon <simon@simon.com>
2024-02-12 16:55:07 +0000

    Added allowed host

--- web_project/settings.py
+++ web_project/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     #'localhost',
     #'127.0.0.1'
-    #'vscode-django-tutorial.azurewebsites.net'  # Sample name only!
+    'django-amazon-group2.onrender.com'
 ]
 
 # Application definition
